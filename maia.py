cbae14c5791e76ef589132b28521656640c07f0f
malaysiaaiassociate <malaysiaaiassociate@gmail.com>
2025-06-27 01:11:07 +0800

    Update maia.py

--- maia.py
+++ maia.py
@@ -10,7 +10,7 @@ genai.configure(api_key=GOOGLE_API_KEY)
 
 new_chat_id = f'{time.time()}'
 MODEL_ROLE = 'ai'
-AI_AVATAR_ICON = 'MAIA'
+AI_AVATAR_ICON = 'Ⓜ️'
 
 # Create a data/ folder if it doesn't already exist
 try:
